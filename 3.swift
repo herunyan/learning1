c714ed5ad11398084e2f6f5d02d5ba7a76d0dc81
herunyan <40029005+herunyan@users.noreply.github.com>
2022-04-24 21:40:07 +0900

    Create 3.swift

---
+++ 3.swift
@@ -0,0 +1,18 @@
+//swift練習
+
+var greeting : String = "Hello world"
+print(greeting)
+greeting = "Hello Swift"
+print(greeting)
+let number : Double = 100
+print(number + 2.3)
+
+// 定数で、複数のmessageを表示しよう
+let greeting : String = "Hello paiza"
+print(greeting)
+
+
+// 定数で、複数のmessageを表示しよう
+let greeting : String = "Hello paiza"
+print(greeting)
+print(greeting)
