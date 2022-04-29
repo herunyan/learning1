e853a6a00867e320d0df41eac77eea2ec483377f
herunyan <40029005+herunyan@users.noreply.github.com>
2022-04-30 01:34:53 +0900

    Create 4.swift

---
+++ 4.swift
@@ -0,0 +1,18 @@
+//swift learning
+//Stringの使い方
+let count = 1000
+print(String(count) + "個")　//print(count + "個")はエラーになる
+
+
+//readLineの使い方
+//ラーニング
+let name = readLine()!
+print("Hello \(name)")
+
+let number = Int(readLine()!)!
+print(number * 50)
+
+//演習問題
+// 整数データを受け取って表示しよう
+let number = Int(readLine()!)!
+print(number)
